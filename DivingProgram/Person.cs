e9e4a3207bbaa56e534b1f949fa016d3f076c2a6
DorMesika <57573065+kozana123@users.noreply.github.com>
2025-02-04 11:43:33 +0200

    check

---
+++ DivingProgram/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivingProgram
+{
+    internal class Person
+    {
+        private string firstName;
+        private string lastName;
+        private string email;
+        private string password;
+        private DateTime birthDate;
+
+        public Person (string firatName, string lastName, string email, string password, DateTime birthDate)
+        {
+            this.firstName = firatName;
+            this.lastName = lastName;
+            this.email = email;
+            this.password = password;
+            this.birthDate = birthDate;
+        }
+
+    
+
+
+
+    }
+}
---
+++ DivingProgram/Rank.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivingProgram
+{
+    internal class Rank
+    {
+        string RankName;
+        int numOfDives;
+        DivingClub club;
+        DateTime dateOfRecipt;
+
+        public Rank(string RankName, int numOfDives, DivingClub club, DateTime dateOfRecipt)
+        {
+            this.RankName = RankName;
+            this.numOfDives = numOfDives;
+            this.club = club;
+            this.dateOfRecipt = dateOfRecipt;
+        }
+        
+
+
+    
+       
+
+       
+    }
+}
