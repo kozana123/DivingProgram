516ab0b243487d40de6d919019fb5ceb2298bd4e
DorMesika <57573065+kozana123@users.noreply.github.com>
2025-02-05 15:52:59 +0200

    jjj

--- DivingProgram/obj/Debug/net8.0-windows/DivingProgram.AssemblyInfo.cs
+++ DivingProgram/obj/Debug/net8.0-windows/DivingProgram.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DivingProgram")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc0b2f867a9b43b1053f5bb9c141386679459129")]
 [assembly: System.Reflection.AssemblyProductAttribute("DivingProgram")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DivingProgram")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
