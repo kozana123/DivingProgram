5a67058449e8f36403da70263977236c29892f0e
DorMesika <57573065+kozana123@users.noreply.github.com>
2025-02-05 16:23:12 +0200

    ggg

---
+++ DivingProgram/Instructor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivingProgram
+{
+    internal class Instructor: Diver
+    {
+        Dictionary <string, (string start, string end)> workingHistory = new Dictionary <string, (string start, string end)>();
+
+        public Instructor(string firstName, string lastName, string email, string password, DateTime birthDate, int diverIdNumber) : base (firstName, lastName, email, password, birthDate, diverIdNumber)
+        {
+
+        }
+    }
+}
